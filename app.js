54e218fd3e92e77ecef99b4b4886cabc87a6e513
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-12-13 09:32:10 -0300

    Using body parser and source paath admin

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http = require("http");
+//Express is like a "backend helper"
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.headers);
-  //   process.exit();
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not Found!</h1>');
 });
 
-server.listen(3000);
+app.listen(3000);
