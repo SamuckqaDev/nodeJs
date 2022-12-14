698460fa0a8d2937b5208053d54ce5af419a6bb2
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-12-13 22:41:58 -0300

    Settinngs in project stucture

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //Express is like a "backend helper"
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,11 +11,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(shopRoutes);
 app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not Found!</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
